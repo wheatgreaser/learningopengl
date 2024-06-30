0d4d338c1943f15616e88cc3a4a3429557bb5fd1
wheatgreaser <wheatgreaser3000@gmail.com>
2024-06-30 23:33:33 +0530

    added vbos

--- learningopengl/main.cpp
+++ learningopengl/main.cpp
@@ -30,6 +30,16 @@ int main() {
 	}
 	glViewport(0, 0, 800, 600); //lower left part of window coords, size
 
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	while (!glfwWindowShouldClose(window))
 	{
